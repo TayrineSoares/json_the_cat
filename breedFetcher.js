5bb90da56b6e8f28c8a9dc85379f06186a00ab8e
TayrineSoares <tayrinecristina@hotmail.com>
2024-11-22 13:14:19 -0500

    Retrives and console.log the breed description

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,20 +2,26 @@ const needle = require('needle');
 
 // Allow the user to specify the breed name using command-line arguments.
 const args = process.argv;
-const breed = args[2];
+const breedName = args[2];
 
 // Retrieves breed from the command line
-const url = ('https://api.thecatapi.com/v1/breeds/search?q=' + breed);
-
+const url = ('https://api.thecatapi.com/v1/breeds/search?q=' + breedName);
 
 
 needle.get(url, (error, response) => {
 
   if (error) {
-    console.error('Error:', error);
-    
-  } else {
-    console.log(typeof response.body);
-    console.log('Body:', response.body);
+    console.error('Error fetching data:', error);
   }
+
+  const data = response.body; 
+  console.log(typeof data); // it is an object
+  console.log(data);  //it is an array of objects
+
+  if (data.length > 0) { 
+    const breed = data[0];
+    console.log('Description:', breed.description);
+    } else {
+      console.log('Breed not found');
+    }
 });
