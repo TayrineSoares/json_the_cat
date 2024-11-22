f1eff75b24eef3a8c2857c7ff7613c72f36391ba
TayrineSoares <tayrinecristina@hotmail.com>
2024-11-22 13:29:35 -0500

    Handle errors

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,8 +1,10 @@
 const needle = require('needle');
 
+// Allow the user to specify the breed name using command-line arguments.
 const args = process.argv;
 const breedName = args[2];
 
+// Retrieves breed from the command line
 const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
 
@@ -10,15 +12,18 @@ needle.get(url, (error, response) => {
 
   if (error) {
     console.error('Error fetching data:', error);
+    return; //The return statement ensures the script stops further execution in the event of an error.
   }
 
-  const data = response.body;
+  const data = response.body; 
+  // console.log(typeof data); // it is an object
+  // console.log(data);  //it is an array of objects
+
   
-  if (data.length > 0) {
-    const breed = data[0];
+  if (data.length > 0) { 
+    const breed = data[0]; // the object that we want is the index 0 of the array
     console.log('Description:', breed.description);
-  } else {
-    console.log('Breed not found');
-  }
-
+    } else {
+      console.log('Breed not found');
+    }
 });
