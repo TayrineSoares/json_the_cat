035063eccea153fda9bf31a51b4d2a20c760e970
TayrineSoares <tayrinecristina@hotmail.com>
2024-11-22 12:48:05 -0500

    Get connection and prints body

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,16 @@
 const needle = require('needle');
+
+const url = 'https://api.thecatapi.com/v1/breeds/search'; 
+
+needle.get(url, (error, response) => {
+
+  if (error) {
+    console.error('Error:', error);
+    
+  } else {
+    console.log(typeof response.body);
+    console.log('Body:', response.body);
+    // console.log('Status Code:', response.statusCode);
+    // console.log('Response Headers:', response.headers);
+  }
+});
